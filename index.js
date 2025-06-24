29b23b9791fd7b90b303ac6231e1da03c236e48e
edem-sed <edemsedzoame@gmail.com>
2025-06-24 12:26:32 +0000

    added limit

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ import { userRouter } from "./routes/user_routes.js"
 import { advertRouter } from "./routes/advert_routes.js"
 
 
-const app = express ()
+const app = express()
 
 const PORT = process.env.PORT || 9191
 app.use(cors())
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/adverts', advertRouter);
@@ -19,6 +19,6 @@ const mongoURI = process.env.MONGO_URI;
 
 await mongoose.connect(mongoURI);
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`server is up on port ${PORT}`)
 })
